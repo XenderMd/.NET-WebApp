787535564a1db35da625075b40623485666ccde1
XenderMd <denis.stavilamd@gmail.com>
2022-01-21 16:09:21 +0100

    Practical jQuery

--- src/DutchTreat/DutchTreat/wwwroot/js/index.js
+++ src/DutchTreat/DutchTreat/wwwroot/js/index.js
@@ -1,16 +1,26 @@
 ﻿
-let form = $("#form");
-form.hide();
+
+$(document).ready(() => {
+
+	let form = $("#form");
+	form.hide();
 
 
-let button = $("buyButton");
-if (button) {
-	button.on("click", (event) => { console.log(event);});
-};
+	let button = $("buyButton");
+	if (button) {
+		button.on("click", (event) => { console.log(event); });
+	};
 
-let listItems = $(".product-props li");
-listItems.on("click", (event) => {
-	console.log("You clicked on " + event.target.innerText);
+	let listItems = $(".product-props li");
+	listItems.on("click", (event) => {
+		console.log("You clicked on " + event.target.innerText);
+	});
+
+	let loginToggle = $("#loginToggle");
+	let popupForm = $(".popupForm");
+
+	loginToggle.on("click", (event) => {
+		popupForm.toggle(1000);
+	});
+
 });
-
-	
