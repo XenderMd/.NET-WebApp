8d8d1270a972fd171a074063177efbe2a6a579c6
XenderMd <denis.stavilamd@gmail.com>
2022-01-21 15:26:35 +0100

    Using Libman - for managing client side files

---
+++ src/DutchTreat/DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,13 @@
+﻿
+let form = document.getElementById('form');
+form.hidden = true;
+
+
+let button = document.getElementById('buyButton');
+if (button) {
+	button.addEventListener("click", (event) => { console.log(event);});
+};
+
+let productInfo = document.getElementsByClassName('product-props');
+let listItems = productInfo.item[0].children;
+	
