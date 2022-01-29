03f7b0d5a2c69ff3bd62f7d0e34b6e8441085664
XenderMd <denis.stavilamd@gmail.com>
2022-01-29 20:55:30 +0100

    Use Identity in the API

--- src/DutchTreat/DutchTreat/Controllers/AccountController.cs
+++ src/DutchTreat/DutchTreat/Controllers/AccountController.cs
@@ -3,10 +3,15 @@ using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DutchTreat.Controllers
@@ -15,11 +20,17 @@ namespace DutchTreat.Controllers
     {
         private readonly ILogger<AccountController> _logger;
         private readonly SignInManager<StoreUser> _singInManager;
+        private readonly UserManager<StoreUser> _usermanager;
+        private readonly IConfiguration _config;
 
-        public AccountController(ILogger<AccountController> logger, SignInManager<StoreUser> singInManager)
+        public AccountController(ILogger<AccountController> logger,
+                                SignInManager<StoreUser> singInManager,
+                                UserManager<StoreUser> usermanager, IConfiguration config)
         {
             _logger = logger;
             _singInManager = singInManager;
+            _usermanager = usermanager;
+            _config = config;
         }
 
         public IActionResult Login()
@@ -59,6 +70,40 @@ namespace DutchTreat.Controllers
         {
             await _singInManager.SignOutAsync();
             return RedirectToAction("Index", "App");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateToken([FromBody] LoginViewModel model) {
+            if (ModelState.IsValid) {
+                var user = await _usermanager.FindByNameAsync(model.Username);
+                if (user != null) {
+                    var result = await _singInManager.CheckPasswordSignInAsync(user, model.Password, false);
+                    if (result.Succeeded) {
+                        //Create the token
+                        var claims = new[] {
+                            new Claim(JwtRegisteredClaimNames.Sub,user.Email),
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                            new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName)
+                        };
+
+                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
+                        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                        var token = new JwtSecurityToken(_config["Tokens:Issuer"],
+                                                         _config["Tokens:Audience"],
+                                                          claims,
+                                                          signingCredentials:creds,
+                                                          expires: DateTime.UtcNow.AddMinutes(20));
+                        return Created("", new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(token),
+                            expiration = token.ValidTo
+                        });
+                    }
+                }
+                
+            }
+
+            return BadRequest();
         }
     }
 }
--- src/DutchTreat/DutchTreat/Startup.cs
+++ src/DutchTreat/DutchTreat/Startup.cs
@@ -6,18 +6,28 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DutchTreat
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -25,6 +35,16 @@ namespace DutchTreat
             services.AddIdentity<StoreUser, IdentityRole>(cfg=> {
                 cfg.User.RequireUniqueEmail = true;
             }).AddEntityFrameworkStores<DutchContext>();
+
+            services.AddAuthentication().AddCookie().AddJwtBearer(cfg=> {
+                cfg.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidIssuer = _config["Tokens:Issuer"],
+                    ValidAudience=_config["Tokens:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]))
+                };
+                                                         
+            });
 
             services.AddDbContext<DutchContext>();
             services.AddTransient<DutchSeeder>();
